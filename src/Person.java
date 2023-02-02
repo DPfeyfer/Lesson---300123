7d5e78eef04d0a20a0ffb7b64459849ed7ea6297
DPfeyfer <Skydenfer737@gmail.com>
2023-02-02 16:23:39 +0300

    Study Lesson 1, 2

--- src/Person.java
+++ src/Person.java
@@ -4,8 +4,8 @@ public class Person {
     int weight;
     String profession;
 
-    public Person(String name, int age, int weight, String profession) {
-        this.name = name;
+    public Person(String names, int age, int weight, String profession) {
+        this.name = names;
         this.age = age;
         this.weight = weight;
         this.profession = profession;
