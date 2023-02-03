d034d71b207014b848510210f9e9f69296a80167
DPfeyfer <Skydenfer737@gmail.com>
2023-02-03 13:05:24 +0300

    Lesson complete

--- src/For_study.java
+++ src/For_study.java
@@ -32,15 +32,17 @@ public class For_study {
     public static void lesson_2() {
         Person denis = new Person("Denchik", 30, 95, "Programmer");
         Person ivan = new Person("Ivan", 50, 85, "Driver");
-        System.out.println("denis.name = " + denis.name);
-        System.out.println("denis.age = " + denis.age);
-        System.out.println("denis.weight = " + denis.weight);
-        System.out.println("denis.profession = " + denis.profession);
+        System.out.println("denis.name = " + denis.getName());
+        System.out.println("denis.age = " + denis.getAge());
+        System.out.println("denis.weight = " + denis.getWeight());
+        System.out.println("denis.profession = " + denis.getProfession());
+        denis.setAge(35);
+        System.out.println("denis.getAge() = " + denis.getAge());
         System.out.println();
-        System.out.println("ivan.name = " + ivan.name);
-        System.out.println("ivan.age = " + ivan.age);
-        System.out.println("ivan.weight = " + ivan.weight);
-        System.out.println("ivan.profession = " + ivan.profession);
+        System.out.println("ivan.name = " + ivan.getName());
+        System.out.println("ivan.age = " + ivan.getAge());
+        System.out.println("ivan.weight = " + ivan.getWeight());
+        System.out.println("ivan.profession = " + ivan.getProfession());
 
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,31 @@
 public class Person {
-    String name;
-    int age;
-    int weight;
-    String profession;
+    private String name;
+    private int age;
+    private int weight;
+    private String profession;
 
     public Person(String name, int age, int weight, String profession) {
         this.name = name;
         this.age = age;
         this.weight = weight;
         this.profession = profession;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+    public int getWeight() {
+        return this.weight;
+    }
+    public String getProfession() {
+        return this.profession;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
